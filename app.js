d2f6a25b3ddbb2aad9a004fc274e14b038257a4b
CHLin13 <lch132502@gmail.com>
2021-12-11 09:50:52 +0800

    20211211 set socket connect by CH

--- app.js
+++ app.js
@@ -10,11 +10,23 @@ const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server)
+
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(passport.initialize())
 app.use(passport.session())
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
