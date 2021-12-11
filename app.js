7e9735dd3a020b2ccfe593e5a94b8c5429b17d05
CHLin13 <lch132502@gmail.com>
2021-12-11 13:08:28 +0800

    20211211 socket set test by CH

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const cors = require('cors')
+const { createServer } = require('http')
+const { Server } = require('socket.io')
 
 const passport = require('./config/passport')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-const http = require('http').Server(app);
-const io = require('socket.io')(http, {
+const http = createServer(app)
+const io = new Server(http, {
   cors: {
     origin: "http://localhost:8080",
     methods: ["GET", "POST"]
   }
-});
+})
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
@@ -31,11 +33,7 @@ io.on('connection', (socket) => {
   })
 })
 
-http.listen(3000, function () {
-  console.log('listening on *:3000');
-});
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+http.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 require('./routes')(app)
 
