ce9b5552618aed024aa2f6b7e4ad2f55068491cf
CHLin13 <lch132502@gmail.com>
2021-12-11 13:26:16 +0800

    20211211 add io.emit by CH

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ app.use(passport.session())
 
 io.on('connection', (socket) => {
   console.log('a user connected')
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
   })
 })
 
