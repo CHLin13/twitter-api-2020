088f7e3cce3baa7b4107be0755fba9cd8025c568
william1993314 <william1993314@gmail.com>
2021-12-12 16:18:52 +0800

    20211212 delete useless code by Will

--- app.js
+++ app.js
@@ -15,25 +15,8 @@ app.use(cors())
 const server = require('http').createServer(app)
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:8081',
+    origin: 'http://localhost:8080',
   }
-})
-console.log("Server socket OK")
-//用 api 方式建立連線
-app.get('/api/messages', function (req, res) {
-  const messages = 'hello world'
-  res.status(200).send(messages)
-})
-
-//用 socket 方式建立連線
-io.on('connection', function (socket) {
-  console.log('======user connected=======', socket.id)
-  // 建立一個 "sendMessage" 的監聽
-  socket.on("sendMessage", function (message) {
-    console.log(message)
-    // 當收到事件的時候，也發送一個 "allMessage" 事件給所有的連線用戶
-    io.emit("allMessage", message)
-  })
 })
 
 server.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- config/socketio.js
+++ config/socketio.js
@@ -7,26 +7,18 @@ module.exports = (io) => {
         socket.on('private message', (msg) => {
             console.log('private_msg====>', msg)
             socket.broadcast.emit('unread_msg', msg)
-            return Promise.all([
                 Message.create({
                     ReceiverId: msg.ReceiverId,
                     message: msg.message,
                     SenderId: msg.SenderId,
                     targetChannel: msg.targetChannel
                 })
-            ])
                 .then(
                     socket.join(msg.targetChannel),
                     io.to(msg.targetChannel).emit('private_msg_from_backend', msg)
                 ).catch(error => {
                     console.log(error)
                 })
-        })
-        socket.on('private chatroom', (channel) => {
-            socket.join(channel)
-        })
-        socket.on('leave private chatroom', (channel) => {
-            socket.leave(channel)
         })
         socket.on('leave', () => {
             console.log('user disconnected')
